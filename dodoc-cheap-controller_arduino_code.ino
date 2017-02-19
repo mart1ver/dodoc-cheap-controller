83bf5c3c2f99c86eba990798d8a52cd3a90c8b76
Vert martin <mart1ver@gmail.com>
2017-02-19 23:02:54 +0100

    Update dodoc-cheap-controller_arduino_code.ino

--- dodoc-cheap-controller_arduino_code.ino
+++ dodoc-cheap-controller_arduino_code.ino
@@ -40,5 +40,5 @@ void loop() {
  // It's better to use DigiKeyboard.delay() over the regular Arduino delay()
  // if doing keyboard stuff because it keeps talking to the computer to make
  // sure the computer knows the keyboard is alive and connected
-  DigiKeyboard.delay(1);
+  DigiKeyboard.delay(90);
 }
