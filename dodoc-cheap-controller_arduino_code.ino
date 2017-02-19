0397d781f124796d3fc9a15c1c71e07895c8ff45
Vert martin <mart1ver@gmail.com>
2017-02-19 17:53:39 +0100

    Update dodoc-cheap-controller_arduino_code.ino

--- dodoc-cheap-controller_arduino_code.ino
+++ dodoc-cheap-controller_arduino_code.ino
@@ -1,14 +1,20 @@
 //dodoc controller v1 martin vert
+
 #include "DigiKeyboard.h"
+
+ char* LeftChar = "w";  //caractere renvoyé par la fleche de gauche (assumes US-style keyboard)
+ char* RightChar = "s"; //caractere renvoyé par la fleche de droite (assumes US-style keyboard)
+ char* SquareChar =  "a"; //caractere renvoyé par le carré rouge (assumes US-style keyboard)
+ 
 
 void setup() {
   pinMode(0, INPUT_PULLUP);// fleche de gauche cablée sur PB0, INPUT_PULLUP, bouton poussoir directement cablé entre cette entrée et la masse, pas besoin de resistance de pull up supplémentaire 
   pinMode(2, INPUT_PULLUP);// fleche de gauche cablée sur PB2 INPUT_PULLUP, bouton poussoir directement cablé entre cette entrée et la masse, pas besoin de resistance de pull up supplémentaire 
   pinMode(1, INPUT);       // carré rouge cablé sur PB1, INPUT simple mais pas besoin de resistance de pull up supplémentaire (présente onboard avec une led en serie), permet l'utilisation de la led à l'appui sur le boutton
-  
 }
 
 void loop() {
+ 
  // lecture de l'etat des boutons 
   int LeftVal = digitalRead(0);
   int RightVal = digitalRead(2);
@@ -19,13 +25,13 @@ void loop() {
   
  // Type out this string letter by letter on the computer (assumes US-style keyboard)
     if (LeftVal == LOW) {
-   DigiKeyboard.print("w");
+   DigiKeyboard.print(LeftChar);
   }
     if (RightVal == LOW) {
-   DigiKeyboard.print("s");
+   DigiKeyboard.print(RightChar);
   }  
     if (SquareVal == HIGH) {
-   DigiKeyboard.print("a");
+   DigiKeyboard.print(SquareChar);
   }
 
  // It's better to use DigiKeyboard.delay() over the regular Arduino delay()
